6342ad47d963080f80f5e6dbf12a0b3e5409c802
Saket Thavanani <saketthavanani@MacBook-Air-5.hitronwifi.local>
2025-11-25 20:32:25 -0500

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ cd ..
 
 echo "Uploading ML Scripts"
 cd ml_pipeline
-aws s3 cp preprocess.py s3://your-bucket/Demo/processing/input/code/preprocess.py
-aws s3 cp evaluation.py s3://your-bucket/Demo/evaluation/input/code/evaluation.py
-aws s3 cp train.py s3://your-bucket/Demo/training/input/code/train.py
+aws s3 cp preprocess.py s3://mlopspipelinestack-sagemakerartifactbucket4252fcb9-fvqyn7tgtetp/Demo/processing/input/code/preprocess.py
+aws s3 cp evaluation.py s3://mlopspipelinestack-sagemakerartifactbucket4252fcb9-fvqyn7tgtetp/Demo/evaluation/input/code/evaluation.py
+aws s3 cp train.py s3://mlopspipelinestack-sagemakerartifactbucket4252fcb9-fvqyn7tgtetp/Demo/training/input/code/train.py
 cd ..
 
 echo "Deploying CDK Stack"
