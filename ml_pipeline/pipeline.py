d066406c94b38d9a7fb20e6252fcb59ca36fc5b5
Saket Thavanani <saketthavanani@MacBook-Air-5.hitronwifi.local>
2025-12-20 16:15:41 -0500

    feature store

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -73,6 +73,7 @@ def get_pipeline(
 
     step_args = sklearn_processor.run(
         job_name='Preprocess Data',
+        dependencies=["requirements.txt"],
         inputs=[
             ProcessingInput(
                 source="s3://mlopspipelinestack-sagemakerartifactbucket4252fcb9-fvqyn7tgtetp/Demo/processing/input/data",
