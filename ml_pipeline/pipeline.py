b4bacc6808cce32de65413d99c3f6f98cb06cd7b
Saket Thavanani <saketthavanani@MacBook-Air-5.local>
2025-12-24 19:44:38 -0500

    new change

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -15,6 +15,10 @@ from sagemaker.processing import FrameworkProcessor, Processor
 
 
 # S3 URIs for preprocessing and evaluation scripts
+S3_PREPROCESSING_URI = (
+    "s3://mlopspipelinestack-sagemakerartifactbucket4252fcb9-fvqyn7tgtetp/"
+    "Demo/processing/input/code/preprocess.py"
+)
 S3_EVALUATION_URI = (
     "s3://mlopspipelinestack-sagemakerartifactbucket4252fcb9-fvqyn7tgtetp/"
     "Demo/evaluation/input/code/evaluation.py"
@@ -54,24 +58,24 @@ def get_pipeline(
 
     # Pipeline parameters for instance counts and types
     processing_instance_count = ParameterInteger(name="ProcessingInstanceCount", default_value=1)
-    processing_instance_type = ParameterString(name="ProcessingInstanceType", default_value="ml.m5.xlarge")
+    processing_instance_type = ParameterString(name="ProcessingInstanceType", default_value="ml.t3.medium")
     training_instance_type = ParameterString(name="TrainingInstanceType", default_value="ml.m4.xlarge")
     model_approval_status = ParameterString(name="ModelApprovalStatus", default_value="Approved")
 
-    custom_processor = Processor(
-        image_uri='252312373833.dkr.ecr.ca-central-1.amazonaws.com/sm-processing-telematics:latest',
-        role=role,
-        sagemaker_session=sagemaker_session,
-        instance_count=processing_instance_count,
-        instance_type=processing_instance_type,
-        base_job_name=f"{base_job_prefix}/custom-preprocess"
 
-    )
+    sklearn_processor = SKLearnProcessor(
+        role=role,
+        framework_version="1.2-1",
+        sagemaker_session=sagemaker_session,
+        instance_count=1,
+        instance_type="ml.t3.medium",
+        base_job_name=f"{base_job_prefix}/sklearn_preprocessor",
+)
 
     # Data preprocessing step
     # NOW use ProcessingStep with step_args (no source_dir here)
     processing_step = ProcessingStep(
-        processor=custom_processor,
+        processor=sklearn_processor,
         name="PreprocessData",
         code='preprocess.py',
         inputs=[
