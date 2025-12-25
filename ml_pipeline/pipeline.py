5ad48941473ee0ff33dfbfecb3bc70e7cf3afb72
Saket Thavanani <saketthavanani@MacBook-Air-5.local>
2025-12-25 14:49:32 -0500

    feature store

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -59,7 +59,7 @@ def get_pipeline(
     # Pipeline parameters for instance counts and types
     processing_instance_count = ParameterInteger(name="ProcessingInstanceCount", default_value=1)
     processing_instance_type = ParameterString(name="ProcessingInstanceType", default_value="ml.t3.medium")
-    training_instance_type = ParameterString(name="TrainingInstanceType", default_value="ml.t3.medium")
+    training_instance_type = ParameterString(name="TrainingInstanceType", default_value="ml.m4.xlarge")
     model_approval_status = ParameterString(name="ModelApprovalStatus", default_value="Approved")
 
 
