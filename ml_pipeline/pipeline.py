0ac318c6e6a41bbdd881ad515639d111e8eb57ea
Saket Thavanani <saketthavanani@MacBook-Air-5.local>
2025-12-24 20:36:52 -0500

    new change

--- ml_pipeline/pipeline.py
+++ ml_pipeline/pipeline.py
@@ -58,7 +58,7 @@ def get_pipeline(
 
     # Pipeline parameters for instance counts and types
     processing_instance_count = ParameterInteger(name="ProcessingInstanceCount", default_value=1)
-    processing_instance_type = ParameterString(name="ProcessingInstanceType", default_value="ml.m5.xlarge")
+    processing_instance_type = ParameterString(name="ProcessingInstanceType", default_value="ml.c6g.xlarge")
     training_instance_type = ParameterString(name="TrainingInstanceType", default_value="ml.m4.xlarge")
     model_approval_status = ParameterString(name="ModelApprovalStatus", default_value="Approved")
 
@@ -77,7 +77,7 @@ def get_pipeline(
         sagemaker_session=sagemaker_session,
         role=role,
         base_job_name=f"{base_job_prefix}/custom-preprocess",
-        instance_type="ml.m5.xlarge",
+        instance_type="ml.c6g.xlarge",
         instance_count=2
 
     )
